d96ec435254ad85f18909c2d388bfc1dd67ae860
cruellaDev <cruellaDevLee@gmail.com>
2025-01-04 14:01:02 +0900

    [solved] 알고리즘 풀이 - 문제08_올바른 괄호

--- java/src/stack/Solution08RightParentheses.java
+++ java/src/stack/Solution08RightParentheses.java
@@ -4,6 +4,8 @@ import java.util.Stack;
 
 public class Solution08RightParentheses {
 
+    // 문제 출처 - [프로그래머스](https://school.programmers.co.kr/learn/courses/30/lessons/12909)
+
     public static void main(String[] args) {
         String input = "(())()";
         long start = System.currentTimeMillis();
